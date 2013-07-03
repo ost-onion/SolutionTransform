82e6bb31e970d18b50fc2ae48bad605106f07699
Brian Scaturro <scaturrob@gmail.com>
2013-07-03 10:32:03 -0400

    Parse projects into a dictionary

--- src/Onion.SolutionTransform/Transformer.cs
+++ src/Onion.SolutionTransform/Transformer.cs
@@ -1,15 +1,33 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Onion.SolutionParser.Parser.Model;
+using Parser = Onion.SolutionParser.Parser.SolutionParser;
 
 namespace Onion.SolutionTransform
 {
     public class Transformer
     {
+        private Dictionary<Guid, Project> _projects; 
+
         public Transformer(string solutionPath)
         {
             if (!File.Exists(solutionPath))
                 throw new FileNotFoundException(string.Format("Solution file {0} does not exist", solutionPath));
             SolutionPath = solutionPath;
             BasePath = new FileInfo(SolutionPath).DirectoryName;
+            _projects = new Dictionary<Guid, Project>();
+        }
+
+        public Dictionary<Guid, Project> GetProjects()
+        {
+            if (_projects.Count > 0) return _projects;
+            var sln = Parser.Parse(SolutionPath);
+            var dict = new Dictionary<Guid, Project>();
+            sln.Projects.ToList().ForEach(i => dict.Add(i.Guid, i));
+            _projects = dict;
+            return _projects;
         }
 
         public string SolutionPath { get; private set; }
--- tests/Onion.SolutionTransform.Tests/TransformerTest.cs
+++ tests/Onion.SolutionTransform.Tests/TransformerTest.cs
@@ -6,6 +6,16 @@ namespace Onion.SolutionTransform.Tests
     [TestFixture]
     public class TransformerTest
     {
+        private string _solutionPath;
+        private Transformer _transformer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _solutionPath = TestUtility.GetFixturePath(@"ndriven\NDriven.sln");
+            _transformer = new Transformer(_solutionPath);
+        }
+
         [Test]
         [ExpectedException(typeof(FileNotFoundException))]
         public void Constructor_should_throw_file_not_found_exception_if_solution_does_not_exist()
@@ -17,17 +27,21 @@ namespace Onion.SolutionTransform.Tests
         [Test]
         public void Constructor_should_set_SolutionPath_if_solution_file_exists()
         {
-            var sln = TestUtility.GetFixturePath(@"ndriven\NDriven.sln");
-            var transform = new Transformer(sln);
-            Assert.AreEqual(sln, transform.SolutionPath);
+            Assert.AreEqual(_solutionPath, _transformer.SolutionPath);
         }
 
         [Test]
         public void Constructor_should_set_BasePath_if_solution_file_exists()
         {
-            var sln = TestUtility.GetFixturePath(@"ndriven\NDriven.sln");
-            var transform = new Transformer(sln);
-            Assert.AreEqual(TestUtility.GetFixturePath("ndriven"), transform.BasePath);
+            Assert.AreEqual(TestUtility.GetFixturePath("ndriven"), _transformer.BasePath);
+        }
+
+        [Test]
+        public void GetProjects_should_parse_solution_and_fetch_projects_and_cache_result()
+        {
+            var projects = _transformer.GetProjects();
+            Assert.True(projects.Count > 0);
+            Assert.AreSame(projects, _transformer.GetProjects());
         }
     }
 }
