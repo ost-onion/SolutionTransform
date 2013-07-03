03b673b29af285c2858a635e318de084b07b7158
Brian Scaturro <scaturrob@gmail.com>
2013-07-03 09:14:34 -0400

    Add fixtures and build events for testing full solution

---
+++ tests/Onion.SolutionTransform.Tests/TransformerTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Onion.SolutionTransform.Tests
+{
+    [TestFixture]
+    public class TransformerTest
+    {
+        public void Constructor_should_throw_file_not_found_exception_if_solution_does_not_exist()
+        {
+            
+        }
+    }
+}
