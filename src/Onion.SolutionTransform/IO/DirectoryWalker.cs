78785e2a096a5818ec3e2f70fa869cfd6ddd356e
Brian Scaturro <scaturrob@gmail.com>
2013-07-03 12:32:14 -0400

    DirectoryWalker gets all files with optional pattern

---
+++ src/Onion.SolutionTransform/IO/DirectoryWalker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Onion.SolutionTransform.IO
+{
+    public static class DirectoryWalker
+    {
+        public static List<string> GetFiles(string path, string pattern = "*.*")
+        {
+            var accum = new List<string>();
+            WalkDirectory(path, pattern, ref accum);
+            return accum;
+        } 
+
+        private static void WalkDirectory(string path, string pattern, ref List<string> accum)
+        {
+            try
+            {
+                accum.AddRange(Directory.GetFiles(path, pattern));
+                foreach (var d in Directory.GetDirectories(path))
+                {
+                    WalkDirectory(d, pattern, ref accum);
+                }
+            }
+            catch (Exception e)
+            {
+                //ignore exceptions
+            }
+        }
+    }
+}
---
+++ tests/Onion.SolutionTransform.Tests/IO/DirectoryWalkerTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Onion.SolutionTransform.IO;
+
+namespace Onion.SolutionTransform.Tests.IO
+{
+    [TestFixture]
+    public class DirectoryWalkerTest
+    {
+        [Test]
+        public void GetFiles_should_return_all_files_in_a_directory()
+        {
+            var files = DirectoryWalker.GetFiles(TestUtility.GetFixturePath("ndriven"));
+            Assert.AreEqual(141, files.Count);
+        }
+
+        [Test]
+        public void GetFiles_should_return_all_files_given_a_wildcard()
+        {
+            var files = DirectoryWalker.GetFiles(TestUtility.GetFixturePath("ndriven"), "*.cs");
+            Assert.AreEqual(74, files.Count);
+        }
+    }
+}
