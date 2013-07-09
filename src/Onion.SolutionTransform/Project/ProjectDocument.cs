6c164f98ba0e76e8a3c861403a9952a3275137db
Brian Scaturro <scaturrob@gmail.com>
2013-07-09 10:53:40 -0400

    ProjectDocument type

---
+++ src/Onion.SolutionTransform/Project/ProjectDocument.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Xml.Linq;
+using Onion.SolutionTransform.Parser;
+
+namespace Onion.SolutionTransform.Project
+{
+    public class ProjectDocument
+    {
+        private TransformableProject _project;
+        private string _path;
+        private readonly XDocument _doc;
+        private static readonly XNamespace XmlNs = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+        public ProjectDocument(TransformableProject proj, IParserInfo info)
+        {
+            _project = proj;
+            _path = info.BasePath + Path.DirectorySeparatorChar + proj.Path;
+            _doc = XDocument.Load(_path);
+        }
+
+        public void Write()
+        {
+            File.WriteAllText(_path, ToString());
+        }
+
+        public override string ToString()
+        {
+            return _doc.ToString();
+        }
+
+        public XElement Project
+        {
+            get { return _doc.Element(XmlNs + "Project"); }
+        }
+
+        public string AssemblyName
+        {
+            get { return Project.Element(XmlNs + "PropertyGroup").Element(XmlNs + "AssemblyName").Value; }
+            set { Project.Element(XmlNs + "PropertyGroup").Element(XmlNs + "AssemblyName").SetValue(value); }
+        }
+
+        public string RootNamespace
+        {
+            get { return Project.Element(XmlNs + "PropertyGroup").Element(XmlNs + "RootNamespace").Value; }
+            set { Project.Element(XmlNs + "PropertyGroup").Element(XmlNs + "RootNamespace").SetValue(value); }
+        }
+    }
+}
--- src/Onion.SolutionTransform/Strategy/ProjectStrategy.cs
+++ src/Onion.SolutionTransform/Strategy/ProjectStrategy.cs
@@ -1,32 +1,19 @@
 ﻿using System.IO;
 using System.Linq;
 using System.Xml.Linq;
+using Onion.SolutionTransform.Project;
 
 namespace Onion.SolutionTransform.Strategy
 {
     public class ProjectStrategy : SolutionTransformStrategyBase
     {
-        private static readonly XNamespace XmlNs = "http://schemas.microsoft.com/developer/msbuild/2003";
-
         public override void Transform()
         {
             var projects = TransformableProjects(p => p.NameIsModified);
             projects.ForEach(p =>
                 {
-                    var path = ParserInfo.BasePath + Path.DirectorySeparatorChar + p.Path;
-                    var doc = XDocument.Load(path);
-                    var project = doc.Element(XmlNs + "Project");
-
-                    //update root namespace and assembly name
-                    var initialPropertyGroup = project.Elements(XmlNs + "PropertyGroup").First();
-                    initialPropertyGroup.Element(XmlNs + "RootNamespace").SetValue(p.Name);
-                    initialPropertyGroup.Element(XmlNs + "AssemblyName").SetValue(p.Name);
-
-                    //update project references
-                    //var all = ParserInfo.GetProjects();
-
-
-                    File.WriteAllText(path, doc.ToString());
+                    var doc = new ProjectDocument(p, ParserInfo) {RootNamespace = p.Name, AssemblyName = p.Name};
+                    doc.Write();
                 });
 
         }
