d61494b8b655c70e7bc57f1862ed333ceb50f065
Brian Scaturro <scaturrob@gmail.com>
2013-07-10 16:13:29 -0400

    Reorder patterns.

--- src/Onion.SolutionTransform/Strategy/CSharpStrategy.cs
+++ src/Onion.SolutionTransform/Strategy/CSharpStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Onion.SolutionTransform.IO;
 using Onion.SolutionTransform.Project;
 using Onion.SolutionTransform.Replacement;
@@ -16,12 +17,12 @@ namespace Onion.SolutionTransform.Strategy
 
         protected override void AddProjectPatterns(TransformableProject p, ISet<IPattern> patterns)
         {
-            var usng = string.Format("using[\\s]+{0}", p.PreviousName);
-            var nspace = string.Format("namespace[\\s]+{0}", p.PreviousName);
-            var qualified = string.Format("\\b{0}\\.", p.PreviousName);
+            var qualified = @"(?<!(using|namespace)[\s]+)" + Regex.Escape(p.PreviousName);
+            var usng = string.Format("using[\\s]+{0}", Regex.Escape(p.PreviousName));
+            var nspace = string.Format("namespace[\\s]+{0}", Regex.Escape(p.PreviousName));
+            patterns.Add(new Pattern(qualified, p.Name));
             patterns.Add(new Pattern(usng, string.Format("using {0}", p.Name)));
             patterns.Add(new Pattern(nspace, string.Format("namespace {0}", p.Name)));
-            patterns.Add(new Pattern(qualified, string.Format("{0}.", p.Name)));
         }
     }
 }
